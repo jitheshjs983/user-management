c2ed18cfe79c5148c55d93174e745149bc3eb08b
jitheshjs983 <jitheshjs983@gmail.com>
2025-06-29 20:02:22 +0530

    env config

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"gorm/conf/controllers"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var dsn = "root:@tcp(127.0.0.1:3306)/test_db?charset=utf8mb4&parseTime=true"
-var db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found. Reading from system env variables.")
+	}
+
+	// Read environment variables
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	// Build DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true",
+		user, pass, host, port, name,
+	)
+
+	// Connect to DB
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
 	handler := &controllers.Handler{DB: db}
 	http.HandleFunc("/register", handler.RegisterUser)
 	http.HandleFunc("/login", handler.LoginUser)
