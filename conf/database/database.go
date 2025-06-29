c1bc38023d70d5e9f1c3ebf0dd913f0852c8553d
jitheshjs983 <jitheshjs983@gmail.com>
2025-06-29 20:15:49 +0530

    config for db connection

--- conf/database/database.go
+++ conf/database/database.go
@@ -1,1 +1,34 @@
 package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found. Reading from system env variables.")
+	}
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true",
+		user, pass, host, port, name)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("❌ Failed to connect to database:", err)
+	}
+
+	DB = db
+}
--- main.go
+++ main.go
@@ -1,41 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"gorm/conf/controllers"
-
-	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"gorm/conf/database"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found. Reading from system env variables.")
-	}
+	database.Init()
 
-	// Read environment variables
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASS")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	name := os.Getenv("DB_NAME")
+	handler := &controllers.Handler{DB: database.DB}
 
-	// Build DSN
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true",
-		user, pass, host, port, name,
-	)
-
-	// Connect to DB
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database:", err)
-	}
-	handler := &controllers.Handler{DB: db}
 	http.HandleFunc("/register", handler.RegisterUser)
 	http.HandleFunc("/login", handler.LoginUser)
 	http.HandleFunc("/change-password", handler.PasswordChange)
